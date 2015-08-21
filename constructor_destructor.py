c8d134f2dbd46fc6da2dfc81ff1a82ea680682de
parthsoni <parth.soni@acudra-it.com>
2015-08-21 16:45:29 +0530

    notes added

---
+++ constructor_destructor.py
@@ -0,0 +1,13 @@
+class Point:
+	def __init(self, x=0, y=0):
+		self.x = x
+		self.y = y
+
+	def __del__(self):
+		class_name = self.__class__.__name__
+		print class_name, "destroyed"
+
+pt1 = Point()
+
+# constructor __del__ is called by "del"
+del pt1
